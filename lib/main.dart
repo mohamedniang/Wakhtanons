3f8c5eccbcfdb9795d057c4dbee341d3f5babdc1
mohamedniang <mouhamedniang1997@gmail.com>
2019-03-17 13:27:08 +0000

    c'est la première fois que je commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wakhtanons',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Wakhtanons !'),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Text('hello start messaging now'),
+              Expanded(
+                child: Container(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Enter destination user',
+                          ),
+                        ),
+                      ),
+                      // Container(
+                      //     child: Flexible(
+                      //         child: ListView(
+                      //           children: <Widget>[
+                      //             const Text('I\'m dedicating every day to you'),
+                      //             const Text('Domestic life was never quite my style'),
+                      //             const Text(
+                      //                 'When you smile, you knock me out, I fall apart'),
+                      //             const Text('And I thought I was so smart'),
+                      //           ],
+                      // ))),
+                      Container(
+                        child: Row(
+                          children: <Widget>[
+                            TextField(
+                              decoration: InputDecoration(
+                                hintText: 'enter your message here',
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.send),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
