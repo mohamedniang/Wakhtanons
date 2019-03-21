ca592b929d5a5d20e76b1af2f4fe96b3b3493454
mohamedniang <mouhamedniang1997@gmail.com>
2019-03-21 17:01:41 +0000

    firebase implementation done

--- lib/main.dart
+++ lib/main.dart
@@ -20,19 +20,36 @@ class MyAppState extends State<MyApp> {
       title: Row(
         children: [
           Expanded(
-            child: Text(document['nom']),
+            child: Text(
+              document['nom'],
+              style: Theme.of(context).textTheme.headline,
+              ),
           ),
           Container(
-            decoration: const BoxDecoration(
-              color: Colors.red,
+            decoration: BoxDecoration(
+              color: Colors.green[100],
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(5.0),
             ),
-            padding: const EdgeInsets.all(10.0),
-            child: Text(document['aime']),
+            padding: const EdgeInsets.all(15.0),
+            child: Text(
+              document['aime'].toString(),
+              style: Theme.of(context).textTheme.display1,
+              ),
           )
         ],
       ),
       onTap: () {
         print("should increse votes here");
+        // document.reference.updateData({
+        //   'aime': document['aime'] + 1
+        // });
+        Firestore.instance.runTransaction((transaction) async {
+          DocumentSnapshot nouveauSnap = await transaction.get(document.reference);
+          await transaction.update(nouveauSnap.reference, {
+            'aime': nouveauSnap['aime'] + 1
+          });
+        });
       },
     );
   }
@@ -49,8 +66,9 @@ class MyAppState extends State<MyApp> {
             stream: Firestore.instance.collection('ladder').snapshots(),
             builder: (context, snapshot) {
               print(snapshot);
-              if (!snapshot.hasData) return const Center(
-                child: Padding(
+              if (!snapshot.hasData)
+                return const Center(
+                    child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Text('Loading ...'),
                 ));
