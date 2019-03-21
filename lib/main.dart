be0f8197370a6bface332e8ed628cb6561d9b327
mohamedniang <mouhamedniang1997@gmail.com>
2019-03-21 14:09:45 +0000

    j'ai enlever tous le code précedent

--- lib/main.dart
+++ lib/main.dart
@@ -6,65 +6,26 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Wakhtanons',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: "test",
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Wakhtanons !'),
+          title: Text("app bar"),
         ),
         body: Center(
-          child: Column(
-            children: <Widget>[
-              Text('hello start messaging now'),
-              Expanded(
-                child: Container(
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: 'Enter destination user',
-                          ),
-                        ),
-                      ),
-                      // Container(
-                      //     child: Flexible(
-                      //         child: ListView(
-                      //           children: <Widget>[
-                      //             const Text('I\'m dedicating every day to you'),
-                      //             const Text('Domestic life was never quite my style'),
-                      //             const Text(
-                      //                 'When you smile, you knock me out, I fall apart'),
-                      //             const Text('And I thought I was so smart'),
-                      //           ],
-                      // ))),
-                      Container(
-                        child: Row(
-                          children: <Widget>[
-                            TextField(
-                              decoration: InputDecoration(
-                                hintText: 'enter your message here',
-                              ),
-                            ),
-                            IconButton(
-                              onPressed: () {},
-                              icon: Icon(Icons.send),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ],
+          child: FlatButton(
+            child: Text("button"),
+            onPressed: () {},
           ),
         ),
       ),
